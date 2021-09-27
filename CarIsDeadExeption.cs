7e644c51c08193cb32773f2b9e100caac6e74f28
Tigris <tabula.tenebris@gmail.com>
2021-09-27 22:19:30 +0600

    commit message

---
+++ CarIsDeadExeption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExeption
+{
+    [Serializable]
+    class CarIsDeadExeption : ApplicationException
+    {
+        //private string messageDetails = String.Empty;
+        //public DateTime ErrorTimeStamp { get; set; }
+        //public string CauseOfError { get; set; }
+        public CarIsDeadExeption() { }
+        public CarIsDeadExeption(string message, string cause, DateTime time):base(message)
+        {
+            //messageDetails = message;
+            //CauseOfError = cause;
+            //ErrorTimeStamp = time;
+        }
+        //public override string Message => $"Car Error Message: {messageDetails}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExeption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(50);
+            }
+            catch (CarIsDeadExeption e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+            Console.WriteLine();
+        }
+    }
+}
